cad9a7738d49f804cf3b6377b90ea3ac8ad61dce
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-02-06 19:02:36 +0800

    Add zodiac symbol selection

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ let iconsInArray = [];
 let icon1 = document.getElementById('icon-1');
 let icon2 = document.getElementById('icon-2');
 let icon3 = document.getElementById('icon-3');
+let viewButton = document.getElementById('view-button');
+let zodiacSelector = document.getElementById('zodiac');
+
+//!!!!!! Add error checking !!!!!
+// Event listeners
+viewButton.addEventListener("click", generateIcons);
 
 
-function applyIcons() {
-    icon1.src = iconsInArray[generateRandomNumber()][1];
-    icon2.src = iconsInArray[generateRandomNumber()][1];
-    icon3.src = iconsInArray[generateRandomNumber()][1];
+function applyIcons(number) {
+    icon1.src = iconsInArray[generateRandomNumber() * number][1];
+    icon2.src = iconsInArray[generateRandomNumber() * number][1];
+    icon3.src = iconsInArray[generateRandomNumber() * number][1];
 
 
 }
@@ -17,7 +23,53 @@ function generateRandomNumber() {
     return parseInt(Math.random() * 100);
 }
 
-async function fetchIcons() {
+function generateIcons() {
+    // Assign selection to zodiac number
+    let zodiacWord = zodiac.value;
+    let zodiacNumber = 1;
+    switch (zodiacWord) {
+        case "taurus":
+            zodiacNumber = 2;
+            break;
+        case "gemini":
+            zodiacNumber = 3;
+            break;
+        case "cancer":
+            zodiacNumber = 4;
+            break;
+        case "leo":
+            zodiacNumber = 5;
+            break;
+        case "virgo":
+            zodiacNumber = 6;
+            break;
+        case "libra":
+            zodiacNumber = 7;
+            break;
+        case "scorpio":
+            zodiacNumber = 8;
+            break;
+        case "sagittarius":
+            zodiacNumber = 9;
+            break;
+        case "capricorn":
+            zodiacNumber = 10;
+            break;
+        case "aquarius":
+            zodiacNumber = 11;
+            break;
+        case "pisces":
+            zodiacNumber = 12;
+            break;
+
+    }
+
+    fetchIcons(zodiacNumber);
+    console.log(zodiacNumber);
+    console.log(zodiacWord);
+}
+
+async function fetchIcons(zodiacNumber) {
     await fetch('https://api.github.com/emojis')
         .then(response => response.json())
         .then(data => icons = data);
@@ -27,8 +79,6 @@ async function fetchIcons() {
     iconsInArray = Object.entries(icons);
 
     console.log(iconsInArray);
-
-    applyIcons();
+    applyIcons(zodiacNumber);
 }
 
-fetchIcons();
